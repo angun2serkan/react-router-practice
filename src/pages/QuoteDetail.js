3dc4066d0005c2dc2a2f38edab1ecda2eb5be2db
Ethem Serkan Angün <serkan.angun@ecospend.com>
2022-10-31 08:17:19 +0300

    QuoteDetail page is edited

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,11 +1,23 @@
 import {useParams, Route} from 'react-router-dom';
+
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
 import Comments from '../components/comments/Comments';
+
+const DUMMY_QUOTES = [
+    {id: 'q1', author: 'Max', text: 'Learning React is Fun!'},
+    {id: 'q2', author: 'Serkan', text: 'Lock your screen!'},
+];
 
 const QuoteDetails = () => {
     const params = useParams();
+
+    const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId);
+
+    if(!quote){
+        return <p>No quote found!</p>;
+    }
     return(<>
-    <h1>Quotes Detail Page!</h1>
-    <p>{params.quoteId}</p>
+    <HighlightedQuote text={quote.text} author={quote.author}></HighlightedQuote>
     <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments></Comments>
     </Route>
