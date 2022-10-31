6a3c87add1178601c222c4b5a95334b5567164bd
Ethem Serkan Angün <serkan.angun@ecospend.com>
2022-10-31 16:46:36 +0300

    lazy loading

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import React, {Suspense} from 'react';
 import {Route, Switch, Redirect} from 'react-router-dom';
 import AllQuotes from './pages/AllQuotes';
-import NewQuote from './pages/NewQuote';
 import QuoteDetails from './pages/QuoteDetail';
 import Layout from './components/layout/Layout';
 import NotFound from './pages/NotFound';
+import LoadingSpinner from './components/UI/LoadingSpinner';
+
+const NewQuote = React.lazy(() => import('./pages/NewQuote'));
+
 
 function App() {
   return (
     <Layout>
+      <Suspense fallback={<div className="centered"><LoadingSpinner></LoadingSpinner></div>}>
     <Switch>
       <Route path='/' exact>
         <Redirect to='/quotes'></Redirect>
@@ -28,6 +33,7 @@ function App() {
         <NotFound></NotFound>
       </Route>
     </Switch>
+      </Suspense>
     </Layout>
   );
 }
